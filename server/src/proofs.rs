108890d548a4281bade4a811fb3e539610ebb591
woocash2 <lukasgniecki@gmail.com>
2024-06-22 23:54:35 +0200

    Errors, logs, comments

--- server/src/proofs.rs
+++ server/src/proofs.rs
@@ -6,6 +6,13 @@ use ark_snark::SNARK;
 use merkle::{MerklePathEntry, NodeType};
 use num_bigint::{BigInt, BigUint};
 
+#[derive(Debug)]
+pub enum Error {
+    BuildProof,
+    GetPublicInputs,
+    Prove,
+}
+
 pub fn generate_clue_proof(
     guess: String,
     solution: String,
@@ -13,7 +20,7 @@ pub fn generate_clue_proof(
     salt: BigUint,
     config: CircomConfig<Bn254>,
     pk: ProvingKey<Bn254>,
-) -> (Proof<Bn254>, [u8; 5]) {
+) -> Result<(Proof<Bn254>, [u8; 5]), Error> {
     let guess = string_to_bigints(guess);
     let solution = string_to_bigints(solution);
 
@@ -23,16 +30,13 @@ pub fn generate_clue_proof(
     builder.push_input("salt", Inputs::BigInt(salt.into()));
     builder.push_input("commit", Inputs::BigInt(commitment.into()));
 
-    let circom = builder.build().unwrap();
+    let circom = builder.build().map_err(|_| Error::BuildProof)?;
 
     // the first five public inputs are actually public outputs
-    let mut inputs = circom.get_public_inputs().unwrap();
+    let mut inputs = circom.get_public_inputs().ok_or(Error::GetPublicInputs)?;
     let _ = inputs.split_off(5);
 
     let mut clue: [u8; 5] = [0, 0, 0, 0, 0];
-
-    println!("{:?}", inputs);
-    println!("{:?}", PrimeField::into_bigint(inputs[0]).to_bytes_le());
 
     for i in 0..inputs.len() {
         if i < 5 {
@@ -44,9 +48,10 @@ pub fn generate_clue_proof(
     let mut rng = rand::thread_rng();
 
     // Generate the proof
-    let proof = Groth16::<Bn254, CircomReduction>::prove(&pk, circom, &mut rng).unwrap();
+    let proof = Groth16::<Bn254, CircomReduction>::prove(&pk, circom, &mut rng)
+        .map_err(|_| Error::Prove)?;
 
-    (proof, clue)
+    Ok((proof, clue))
 }
 
 pub fn generate_membership_proof(
@@ -56,7 +61,7 @@ pub fn generate_membership_proof(
     path: Vec<MerklePathEntry>,
     config: CircomConfig<Bn254>,
     pk: ProvingKey<Bn254>,
-) -> Proof<Bn254> {
+) -> Result<Proof<Bn254>, Error> {
     let solution = string_to_bigints(solution);
     let mut builder = CircomBuilder::new(config);
 
@@ -77,13 +82,31 @@ pub fn generate_membership_proof(
     builder.push_input("hashes", Inputs::BigIntVecVec(hashes));
     builder.push_input("pathIndicators", Inputs::BigIntVec(indicators));
 
-    let circom = builder.build().unwrap();
+    let circom = builder.build().map_err(|_| Error::BuildProof)?;
 
     // Generate the proof
     let mut rng = rand::thread_rng();
-    Groth16::<Bn254, CircomReduction>::prove(&pk, circom, &mut rng).unwrap()
+    Groth16::<Bn254, CircomReduction>::prove(&pk, circom, &mut rng).map_err(|_| Error::Prove)
 }
 
 fn string_to_bigints(s: String) -> Vec<BigInt> {
     s.as_bytes().iter().map(|x| (x - 97).into()).collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::string_to_bigints;
+
+    #[test]
+    fn string_to_bigints_correct() {
+        let word: String = "hello".into();
+        let bigints = string_to_bigints(word.clone());
+
+        assert_eq!(bigints.len(), word.len());
+        assert_eq!(bigints[0], 7.into()); //  h
+        assert_eq!(bigints[1], 4.into()); //  e
+        assert_eq!(bigints[2], 11.into()); // l
+        assert_eq!(bigints[3], 11.into()); // l
+        assert_eq!(bigints[4], 14.into()); // o
+    }
 }
